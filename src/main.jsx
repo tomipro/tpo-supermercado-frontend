8bc227115f240549d36378b263e4cddfd90a18bc
tomipro <tomy.prodan@gmail.com>
2025-07-02 00:55:39 -0300

    ultimo detallito

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './auth/AuthProvider.jsx'
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -11,9 +10,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <AuthProvider>
          <App />
-        </AuthProvider>
       </BrowserRouter>
     </Provider>
   </StrictMode>,
